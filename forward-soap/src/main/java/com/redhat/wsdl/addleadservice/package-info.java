2ea1245d09ee81dabc420f0145d18c4bedc00797
Simon <sigreen@redhat.com>
2019-04-17 12:47:53 -0400

    Rollback changes:

--- forward-soap/src/main/java/com/redhat/wsdl/addleadservice/package-info.java
+++ forward-soap/src/main/java/com/redhat/wsdl/addleadservice/package-info.java
@@ -1,2 +1,2 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://redhat.com/wsdl/AddLeadService/")
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://redhat.com/wsdl/AddLeadService/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.redhat.wsdl.addleadservice;
--- soap-endpoint/src/main/java/com/redhat/AddLeadService.java
+++ soap-endpoint/src/main/java/com/redhat/AddLeadService.java
@@ -2,8 +2,6 @@ package com.redhat;
 
 import javax.jws.WebParam;
 import javax.jws.WebService;
-import javax.jws.WebMethod;
-import javax.jws.soap.SOAPBinding;
 
 import com.redhat.model.LeadInfo;
 
@@ -13,10 +11,8 @@ import com.redhat.model.LeadInfo;
 		  serviceName = "AddLeadService",
 		  portName = "SOAPOverHTTP"
 		)
-@SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface AddLeadService {
 	
-	@WebMethod
 	public LeadInfo addLead(@WebParam(name = "first_name", targetNamespace = "") String first_name, @WebParam(name = "last_name", targetNamespace = "") String last_name, @WebParam(name = "company", targetNamespace = "") String company, @WebParam(name = "source", targetNamespace = "") String source, @WebParam(name = "phone", targetNamespace = "") String phone, @WebParam(name = "email", targetNamespace = "") String email,
 			@WebParam(name = "status", targetNamespace = "") String status, @WebParam(name = "rating", targetNamespace = "") String rating);
 
